261cb9c8826c56c074cc2210e3395b4eb100354b
misha <misha7877@gmail.com>
2024-02-19 10:37:40 +0500

    - написал интеграционный тест

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('receive-event', [AccountEventController::class, 'receiveEvent']);
+Route::post('receive-event', [AccountEventController::class, 'receiveEvent'])
+    ->name('account-event.event-receive');
